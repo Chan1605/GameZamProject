d3e3f2f80596274aba89c94872a2500ba379cd7d
goodeun <goodeun10.1@gmail.com>
2026-08-28 17:58:05 +0900

    작업중

--- Assets/3.Script/Rank/SortRank.cs
+++ Assets/3.Script/Rank/SortRank.cs
@@ -10,6 +10,8 @@ public class SortRank : MonoBehaviour
 
     private SaveData saveData;
 
+    public Record curRecord;
+
     // 이번 플레이어의 기록을 생성한 후, 삽입 정렬 방식으로 기존 기록에 넣은 후 덮어씌우기
     // 인덱스 기반으로 관리
 
@@ -20,7 +22,29 @@ public class SortRank : MonoBehaviour
 
     private void InsertSort()
     {
+        saveData.saveRecords.Add(curRecord);
 
+        if (saveData.saveRecords.Count <= 1) // 기록에 자신뿐이라면
+        {
+            return;
+        }
+
+        //아니면 비교 시작
+        for (int i = saveData.saveRecords.Count - 1; i > 0; i--) // 끝부터 시작 (지금 들어온 기록)
+        {
+            Record pastRank = saveData.saveRecords[i-1];
+
+            if(pastRank.score < curRecord.score) // 지금 들어온 기록 i 보다 i-1의 기록이 작다면 뒤로 보낸다.
+            {
+
+            }
+            else
+            {
+                break; // 지금 들어온 기록 i 보다 i-1의 기록이 크다면 반복 끝
+            }
+
+
+        }
     }
 
 
