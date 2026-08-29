c0e612ab46881beaa6962d306df98ca6fe61e49f
mgkim0518 <76196615+mgkim0518@users.noreply.github.com>
2026-08-29 13:37:02 +0900

    SkillQ + InputAction

--- Assets/3.Script/Skill_Q.cs
+++ Assets/3.Script/Skill_Q.cs
@@ -22,9 +22,8 @@ public class Skill_Q : MonoBehaviour
 
         Rigidbody carriage = consist.Locomotive.Body;
         Quaternion targetRot = carriage.rotation; //객차의 회전
-        Vector3 chainAnchor = carriage.position; //객차의 위치
-
-        TrainCar previous = consist.Locomotive;
+        Vector3 chainAnchor = carriage.position + targetRot * consist.Locomotive.RearAnchorLocal
+                     - (targetRot * Vector3.forward) * consist.Gap; //다음 차량이 붙어야 할 좌표
 
         foreach (TrainCar car in consist.Cars)
         {
@@ -39,17 +38,18 @@ public class Skill_Q : MonoBehaviour
             }
 
             //객차들의 위치를 이전 객차들을 기준으로 이동
-            Vector3 desiredAnchor = previous.Body.position 
-                + previous.Body.rotation * previous.RearAnchorLocal
-                - (carriage.rotation * Vector3.forward) * consist.Gap;
-            Vector3 currentAnchor = car.Body.position + car.Body.rotation * car.FrontAnchorLocal;
-
-            Vector3 correction = desiredAnchor - currentAnchor;
-            car.Body.MovePosition(car.Body.position + correction);
+            Vector3 currentAnchor = car.Body.position + targetRot * car.FrontAnchorLocal;
+            Vector3 newPos = car.Body.position + (chainAnchor - currentAnchor);
+            //newPos로 따로 저장해서 지연 없이 확실하게 반영되게 만듦
+            car.Body.MovePosition(newPos);
 
             //객차들의 회전을 기관차와 동일하게 만들기
             car.Body.MoveRotation(targetRot);
-            
+
+            //다음 차량이 붙어야 할 자리를 계산함
+            chainAnchor = newPos + targetRot * car.RearAnchorLocal - (targetRot * Vector3.forward) * consist.Gap;
+
+
         }
 
     }
