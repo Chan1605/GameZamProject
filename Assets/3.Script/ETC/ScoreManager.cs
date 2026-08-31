25e205d2a6ef73469e6a1362c285ddff91b12e05
Chan1605 <105186869+Chan1605@users.noreply.github.com>
2026-08-31 15:41:57 +0900

    [Add]SoundMgr
    사운드 매니저
    빈 오브젝트 필요없이 아래와 같이 호출하면 됩니다.     //SoundMgr.Instance.PlayEffSound("SFX_UI_Button_Click_Settings_2", 0.5f);
    ScoreManager - Null방지 추가

--- Assets/3.Script/ETC/ScoreManager.cs
+++ Assets/3.Script/ETC/ScoreManager.cs
@@ -14,6 +14,15 @@ public class ScoreManager : MonoBehaviour
         RefreshUI();
     }
 
+
+    private void OnDestroy()
+    {
+        if (SaveManager.Instance != null) //방어코드 추가
+        {
+            SaveManager.Instance.PlayerScore = Score;
+        }
+    }
+
     public void AddScore(int amount)
     {
         Score += amount;
